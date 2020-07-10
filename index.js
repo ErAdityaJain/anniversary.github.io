06bb7c8f746b88a1d1ccc9dbf08e081c0d3db18a
Aditya jain <55448649+ErAdityaJain@users.noreply.github.com>
2020-07-10 15:27:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -295,7 +295,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/_z-1fTlSDF0?controls=0&loop=1&autoplay=1");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/251BSan1lW4?controls=0&loop=1&autoplay=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
